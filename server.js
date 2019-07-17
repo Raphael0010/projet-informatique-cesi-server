42cc09f363f305662b7420ebdf005ae62f4973a9
Raphael Mortier <raphaelmortier@outlook.Fr>
2019-07-17 13:56:25 +0200

    Work In Progress 🚀

--- server.js
+++ server.js
@@ -15,14 +15,14 @@ io.on("connection", (socket) => {
             socket.emit("cmd return",stdout);
         });
     })
-    socket.on("snmp_get_ip", (cmd) => {
+    socket.on("raspi_get_temp", (cmd) => {
         console.log("Handling request : " + cmd);
-        exec("snmpwalk -v 2c -c public localhost iso.3.6.1.2.1.92.1.3.2.1.9.7.100.101.102.97.117.108.116.1.2 -Ov -Oq        ", (error, stdout, stderr) => {
+        exec("/opt/vc/bin/vcgencmd measure_temp", (error, stdout, stderr) => {
             if (error) {
                 socket.emit("cmd return","wrongcmd");
                 return;
             }
-            socket.emit("snmp_get_ip_return",stdout);
+            socket.emit("raspi_get_temp_return",stdout);
         });
     })
 })
